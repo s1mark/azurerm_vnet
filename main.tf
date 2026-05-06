f1041b3d3a39ac847975013e6dea239c6beef577
Mark Simon <33624175+s1mark@users.noreply.github.com>
2026-05-06 13:15:48 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,16 +5,21 @@ variable "vnet_address_space"    {}
 variable "subnet_name"           {}
 variable "subnet_address_prefix" {}
 
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
   address_space       = [var.vnet_address_space]
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
-  resource_group_name  = var.resource_group_name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.subnet_address_prefix]
 }
